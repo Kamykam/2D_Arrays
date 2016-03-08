7c5ec16b04df58003f82c7d235bac8053e533bce
Kamran Thomas Alimagham <kamranthomas@h167.eduroam-2a.wlan.aalborguniversitet.dk>
2016-03-08 13:33:54 +0100

    Initial Commit

---
+++ 2D_Arrays/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  2D_Arrays
+//
+//  Created by Kamran Alimagham on 08/03/2016.
+//  Copyright © 2016 AAU. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
